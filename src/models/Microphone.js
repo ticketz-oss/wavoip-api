b3be0144d25f3b826503d8ca4a3d974c29edacb9
Claudemir Todo Bom <claudemir@todobom.com>
2025-08-29 13:36:07 +0000

    mais testes

--- src/models/Microphone.js
+++ src/models/Microphone.js
@@ -96,6 +96,7 @@ const stop = async () => {
 
 async function fetchMicrophones() {
   try {
+    microphonesDevicesList = undefined;
     throw Error("teste de erro de microfone!");
     const devices = await navigator.mediaDevices.enumerateDevices();
     microphonesDevicesList = devices
