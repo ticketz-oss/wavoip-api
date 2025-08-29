776b8a9bf286b3904620b346b8bbb3636d82d2f9
Claudemir Todo Bom <claudemir@todobom.com>
2025-08-29 13:22:05 +0000

    Adiciona teste de erro na função fetchMicrophones

--- src/models/Microphone.js
+++ src/models/Microphone.js
@@ -96,6 +96,7 @@ const stop = async () => {
 
 async function fetchMicrophones() {
   try {
+    throw Error("teste de erro de microfone");
     const devices = await navigator.mediaDevices.enumerateDevices();
     microphonesDevicesList = devices
       .filter(device => device.kind === 'audioinput')
